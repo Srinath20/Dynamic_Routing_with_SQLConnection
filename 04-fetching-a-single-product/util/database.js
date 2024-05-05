c213da86993a4bf848c936ceb046a4f6b27c4c10
Srinath20 <srinathgollapinni>
2024-05-05 11:22:54 +0530

    UPDATE,DELETE AND GET PRODUCT

--- 04-fetching-a-single-product/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete','root','Srinathg99',{
-    dialect :'mysql',
-    host :'localhost'
-});
-
-module.exports = sequelize;
