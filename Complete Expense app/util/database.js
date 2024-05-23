d2bcd2217d0741aeaeac3d5678c938cb70f145bc
Srinath20 <srinathgollapinni>
2024-05-23 23:00:52 +0530

    Complete full stack app

---
+++ Complete Expense app/util/database.js
@@ -0,0 +1,17 @@
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete2',
+//     password:'Srinathg99'
+// })
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete2','root','Srinathg99',{
+    dialect:'mysql',
+    host:'localhost'
+});
+
+module.exports = sequelize;
